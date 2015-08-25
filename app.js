17e1c4d65ca2e537bbedafd113f1205d8d3bb29a
Haike Zegwaard <h.zegwaard@gmail.com>
2015-08-25 16:15:42 +0200

    Update app.js
    add test function for controller

--- app.js
+++ app.js
@@ -6,3 +6,26 @@ myApp.controller('MyController', function($scope) {
                    {"name":"habanero", "spiciness":"LAVA HOT!!"}];
   $scope.spice = "habanero";
 });
+
+//Controller test:
+describe('myController function', function() {
+
+  describe('myController', function() {
+    var $scope;
+
+    beforeEach(module('myApp'));
+
+    beforeEach(inject(function($rootScope, $controller) {
+      $scope = $rootScope.$new();
+      $controller('MyController', {$scope: $scope});
+    }));
+
+    it('should create "spices" model with 3 spices', function() {
+      expect($scope.spices.length).toBe(3);
+    });
+
+    it('should set the default value of spice', function() {
+      expect($scope.spice).toBe('habanero');
+    });
+  });
+});
