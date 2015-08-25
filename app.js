932e267fff5658e56868560928cb9e9a44b56700
Haike Zegwaard <h.zegwaard@gmail.com>
2015-08-25 16:04:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-var myApp = angular.module('scopeInheritance', []);
-myApp.controller('MainController', ['$scope', function($scope) {
-  $scope.timeOfDay = 'morning';
-  $scope.name = 'Nikki';
-}]);
-myApp.controller('ChildController', ['$scope', function($scope) {
-  $scope.name = 'Mattie';
-}]);
-myApp.controller('GrandChildController', ['$scope', function($scope) {
-  $scope.timeOfDay = 'evening';
-  $scope.name = 'Gingerbread Baby';
-}]);
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('MyController', function($scope) {
+  $scope.spices = [{"name":"pasilla", "spiciness":"mild"},
+                   {"name":"jalapeno", "spiciness":"hot hot hot!"},
+                   {"name":"habanero", "spiciness":"LAVA HOT!!"}];
+  $scope.spice = "habanero";
+});
